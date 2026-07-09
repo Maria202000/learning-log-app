91ba53e29062fba4c11787507ef55405ea980e7f
Maria202000 <g2432042@tcu.ac.jp>
2026-07-09 16:12:23 +0900

    Update collector-config.js

--- collector-config.js
+++ collector-config.js
@@ -1,3 +1,3 @@
 // Google Apps Script の「ウェブアプリ URL」をここに貼ります。
 // 例: https://script.google.com/macros/s/XXXXXXXXXXXXXXXXXXXXXXXXXXXX/exec
-window.LEARNING_LOG_COLLECTOR_URL = "";
+window.LEARNING_LOG_COLLECTOR_URL = "https://script.google.com/macros/s/AKfycbwSEQNIZ9V0srGSs4nX0UiftckMA7YuxpCZN5U5wnoyczrFeYSBt05cvgjm9uh0hYll_Q/exec";
