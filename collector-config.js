5570e49b08c19dbd044c31f574c0e702acc3993f
Maria202000 <g2432042@tcu.ac.jp>
2026-07-16 14:18:30 +0900

    Add files via upload

--- collector-config.js
+++ collector-config.js
@@ -1,3 +1,2 @@
-// Google Apps Script の「ウェブアプリ URL」をここに貼ります。
-// 例: https://script.google.com/macros/s/XXXXXXXXXXXXXXXXXXXXXXXXXXXX/exec
-window.LEARNING_LOG_COLLECTOR_URL = "https://script.google.com/macros/s/AKfycbwSEQNIZ9V0srGSs4nX0UiftckMA7YuxpCZN5U5wnoyczrFeYSBt05cvgjm9uh0hYll_Q/exec";
+// Paste the Google Apps Script web app URL here.
+window.LEARNING_LOG_COLLECTOR_URL = "https://script.google.com/macros/s/AKfycbxsWyi8AoqHsabbseQq3I4QVTWZIDgFZ5QroAY-XoYtvrUZ5FyMXNJXYOI_PZIJP1TQ/exec";
